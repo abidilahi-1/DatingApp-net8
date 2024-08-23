6e7f2745a8ac3a0f0f849554dea238877047ed4c
abidilahi-1 <abid.ilahi1@outlook.com>
2024-08-23 09:04:46 +0530

    End of section 4

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public required string UserName { get; set; }
+    [Required]
+    public required string password { get; set; }
+    
+}
